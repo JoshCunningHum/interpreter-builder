560dd752b1653d6df8f711b8312861a75d02dec0
JoshCunningHum <jkdimpas@gmail.com>
2024-04-06 23:27:28 +0800

    updated parser logic
    match then map instead of match-all then map-all

--- src/logic/ast.ts
+++ src/logic/ast.ts
@@ -14,63 +14,93 @@ import genid from "@/utils/genid";
 
 const DEV = false;
 
-// const RunRule = (
-//     pool: ParsePoolItem[],
-//     rules: ParseRule[],
-//     tokenDefs: TokenDef[],
+// export const produceAST = (
+//     params: ReturnType<typeof PrepareParser>,
+
+//     // Recursive Parameters
 //     ruleIndex = 0,
-//     Logging: boolean = false,
-//     Logs: ParseProcessLog[] = [],
 //     MAX_RECURSION_GUARD = 100,
 // ): {
-//     pool: (Token | ASTNode)[];
+//     pool: ParsePoolItem[];
 //     history?: ParseProcessLog[];
-//     error?: string;
 // } => {
-//     // Base Case
-//     if (ruleIndex > rules.length - 1 || MAX_RECURSION_GUARD <= 0)
-//         return { pool, history: Logs };
+//     const {
+//         pool,
+//         rules,
+//         tokenDefs,
+//         log,
+//         history,
+//         onEvalError,
+//         onError,
+//         T,
+//         TX,
+//     } = params;
 
-//     const rule = rules[rules.length - (ruleIndex + 1)];
-//     if (DEV)
-//         console.log(
-//             `%cStarting iteration for %c<${rule.name}>`,
-//             "color:cyan",
-//             "color:teal",
+//     const output = { pool, history };
+//     const runtimeLog: any[] = [];
+//     const _execution_id = genid(16);
+//     const _onError = (msg: string, line: number, column: number) =>
+//         onError(msg, line, column, _execution_id);
+
+//     // Base Case
+//     if (ruleIndex > rules.length - 1) return output;
+
+//     const rule = rules[ruleIndex];
+
+//     if (MAX_RECURSION_GUARD <= 0) {
+//         onEvalError(
+//             new Error("MAX_RECURSION_GUARD Exceeded"),
+//             "other",
+//             _execution_id,
 //         );
+
+//         if (log) {
+//             history.push({
+//                 id: _execution_id,
+//                 rule,
+//                 ruleIndex,
+//                 matches: [],
+//                 pool: JSON.parse(JSON.stringify(pool)),
+//                 logs: runtimeLog,
+//             });
+//         }
+
+//         return output;
+//     }
 
 //     // Run Matching
 //     const matches: [number, number][] = [];
 
-//     let MAX_LOOP = pool.length * 2;
+//     let MAX_LOOP = pool.length + 3;
 //     while (MAX_LOOP--) {
-//         const start = matches.reduce(
-//             (max, [s, n]) => (max > s + n ? max : s + n),
-//             0,
+//         const offset = matches.reduce((max, [s, n]) => Math.max(max, s + n), 0);
+//         if (offset > pool.length - 1) break;
+
+//         const args = RuleMatchArgsBuilder(
+//             {
+//                 history,
+//                 log,
+//                 rules,
+//                 T,
+//                 tokenDefs,
+//                 TX,
+//                 onEvalError,
+//                 // Changed
+//                 pool: pool.slice(offset),
+//                 onError: _onError,
+//             },
+//             runtimeLog,
 //         );
-
-//         if (start > pool.length - 1) break;
-
-//         const args = RuleMatchArgsBuilder(pool.slice(start), tokenDefs, rules);
-
 //         const result = RunParseMatch(rule, args, (e) => {
-//             console.error(`Error on parsing rule: ${rule.name}`, e);
+//             onEvalError(e, "match", _execution_id);
 //         });
 
-//         if (DEV) console.log(rule.name, start, args, result, matches);
+//         if (!result) break;
 
-//         if (((r: any): r is [number, number] => Array.isArray(r))(result)) {
-//             const [_start, _n] = result;
-//             if (_start === -1) break;
-//             matches.push([_start + start, _n]);
-//         } else if (result instanceof Error) {
-//             return { pool, error: result.message };
-//         } else if (result === undefined) break;
+//         const [start, length] = result;
+//         if (start === -1) break;
+//         matches.push([offset + start, length]);
 //     }
-
-//     if (matches.length && DEV) console.log(matches);
-//     else if (DEV)
-//         console.log("%cNo Matches in this iteration", "color:crimson");
 
 //     // Run Mapper
 //     matches.reduce((offset, [s, l]) => {
@@ -78,60 +108,43 @@ const DEV = false;
 
 //         // Generate RuleMapperArgs
 //         const args = RuleMapperArgsBuilder(
-//             pool,
+//             params,
 //             [start, start + l],
 //             rule,
-//             tokenDefs,
+//             runtimeLog,
 //         );
 
-//         // Pass it to the mapper
-//         const error = RunParseMapper(rule, args, (e) =>
-//             console.error(`Error on mapping: ${rule.name}`, e),
-//         );
-
+//         // Pass to a mapper
+//         RunParseMapper(rule, args, (e) => {
+//             onEvalError(e, "map", _execution_id);
+//         });
 //         const generatedNode = args.data();
 
-//         pool.splice(start, l, generatedNode);
-
-//         return -(l - 1);
+//         pool.splice(start, l, ...generatedNode);
+//         return offset - (l - 1);
 //     }, 0);
 
-//     if (Logging) {
-//         Logs.push({
-//             id: genid(16),
+//     if (log) {
+//         history.push({
+//             id: _execution_id,
 //             rule,
 //             ruleIndex,
 //             matches,
-//             pool: structuredClone(pool),
+//             pool: JSON.parse(JSON.stringify(pool)),
+//             logs: runtimeLog,
 //         });
 //     }
 
-//     if (matches.length && DEV)
-//         console.log(
-//             `%cAfterMapping: %c[${pool.map((v) => ("kind" in v ? v.kind : tokenDefs.find((def) => def.id === v.type)?.name))}]`,
-//             "color:lightblue",
-//             "color:turquoise",
-//         );
-
-//     // If matches is not empty, then rerun the whole thing, if not then rerun but to the next ruleIndex
-//     return RunRule(
-//         pool,
-//         rules,
-//         tokenDefs,
+//     return produceAST(
+//         params,
 //         matches.length > 0 ? 0 : ruleIndex + 1,
-//         Logging,
-//         Logs,
-//         MAX_RECURSION_GUARD - 1,
+//         matches.length > 0 ? MAX_RECURSION_GUARD - 1 : 100,
 //     );
-//     // return { pool }
 // };
 
-export const produceAST = (
+// Rewrite of the parser processing into iterative Map every Match instead of Match all then Map all
+export const _produceAST = (
     params: ReturnType<typeof PrepareParser>,
-
-    // Recursive Parameters
-    ruleIndex = 0,
-    MAX_RECURSION_GUARD = 100,
 ): {
     pool: ParsePoolItem[];
     history?: ParseProcessLog[];
@@ -142,117 +155,98 @@ export const produceAST = (
         tokenDefs,
         log,
         history,
-        onEvalError,
-        onError,
         T,
         TX,
+        onError: _onError,
+        onEvalError,
     } = params;
 
     const output = { pool, history };
-    const runtimeLog: any[] = [];
-    const _execution_id = genid(16);
-    const _onError = (msg: string, line: number, column: number) =>
-        onError(msg, line, column, _execution_id);
+    let ruleindex = 0;
 
-    // Base Case
-    if (ruleIndex > rules.length - 1) return output;
+    // Loop through all rule definition
+    while (ruleindex < rules.length) {
+        const rule = rules[ruleindex];
+        const execution_id = genid(16);
 
-    const rule = rules[ruleIndex];
+        const onError = (msg: string, line: number, column: number) =>
+            _onError(msg, line, column, execution_id);
 
-    if (MAX_RECURSION_GUARD <= 0) {
-        onEvalError(
-            new Error("MAX_RECURSION_GUARD Exceeded"),
-            "other",
-            _execution_id,
-        );
+        const runtimelog: any[] = [];
+        const matches: [number, number][] = [];
+
+        // Loop until there are no matches
+        let MAX_LOOP = pool.length + 1; // infinite loop guard
+        while (MAX_LOOP--) {
+            const match_args = RuleMatchArgsBuilder(
+                {
+                    history,
+                    log,
+                    rules,
+                    T,
+                    tokenDefs,
+                    TX,
+                    onEvalError,
+                    pool,
+                    onError,
+                },
+                runtimelog,
+            );
+
+            const match_result = RunParseMatch(rule, match_args, (e) =>
+                onEvalError(e, "match", execution_id),
+            );
+
+            if (!match_result) break;
+
+            const [start, length] = match_result;
+            if (start === -1) break;
+
+            matches.push([start, length]);
+
+            const map_args = RuleMapperArgsBuilder(
+                params,
+                [start, start + length],
+                rule,
+                runtimelog,
+            );
+
+            RunParseMapper(rule, map_args, (e) =>
+                onEvalError(e, "map", execution_id),
+            );
+
+            const map_result = map_args.data();
+
+            pool.splice(start, length, ...map_result);
+        }
 
         if (log) {
             history.push({
-                id: _execution_id,
+                id: execution_id,
                 rule,
-                ruleIndex,
-                matches: [],
+                ruleIndex: ruleindex,
+                matches,
                 pool: JSON.parse(JSON.stringify(pool)),
-                logs: runtimeLog,
+                logs: runtimelog,
             });
         }
 
-        return output;
+        ruleindex = matches.length > 0 ? 0 : ruleindex + 1;
     }
 
-    // Run Matching
-    const matches: [number, number][] = [];
-
-    let MAX_LOOP = pool.length + 3;
-    while (MAX_LOOP--) {
-        const offset = matches.reduce((max, [s, n]) => Math.max(max, s + n), 0);
-        if (offset > pool.length - 1) break;
-
-        const args = RuleMatchArgsBuilder(
-            {
-                history,
-                log,
-                rules,
-                T,
-                tokenDefs,
-                TX,
-                onEvalError,
-                // Changed
-                pool: pool.slice(offset),
-                onError: _onError,
-            },
-            runtimeLog,
-        );
-        const result = RunParseMatch(rule, args, (e) => {
-            onEvalError(e, "match", _execution_id);
-        });
-
-        if (!result) break;
-
-        const [start, length] = result;
-        if (start === -1) break;
-        matches.push([offset + start, length]);
-    }
-
-    // Run Mapper
-    matches.reduce((offset, [s, l]) => {
-        const start = offset + s;
-
-        // Generate RuleMapperArgs
-        const args = RuleMapperArgsBuilder(
-            params,
-            [start, start + l],
-            rule,
-            runtimeLog,
-        );
-
-        // Pass to a mapper
-        RunParseMapper(rule, args, (e) => {
-            onEvalError(e, "map", _execution_id);
-        });
-        const generatedNode = args.data();
-
-        pool.splice(start, l, ...generatedNode);
-        return offset - (l - 1);
-    }, 0);
-
-    if (log) {
-        history.push({
-            id: _execution_id,
-            rule,
-            ruleIndex,
-            matches,
-            pool: JSON.parse(JSON.stringify(pool)),
-            logs: runtimeLog,
-        });
-    }
-
-    return produceAST(
-        params,
-        matches.length > 0 ? 0 : ruleIndex + 1,
-        matches.length > 0 ? MAX_RECURSION_GUARD - 1 : 100,
-    );
+    return output;
 };
+
+export const produceAST = (
+    params: ReturnType<typeof PrepareParser>,
+
+    // Recursive Parameters
+    ruleIndex = 0,
+    MAX_RECURSION_GUARD = 100,
+): {
+    pool: ParsePoolItem[];
+    history?: ParseProcessLog[];
+} => _produceAST(params);
 
 export const checkASTHealth = (pool: ParsePoolItem[]): boolean => {
     if (!pool) return false;
